5c5fe342bdf398f9471e08a624aa2f3c94aafbe6
LazyHubBot <131406457+LazyHubBot@users.noreply.github.com>
2023-04-21 03:12:24 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
-SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = -1001884373226
-ADMIN        = 1350642629
-CHANNEL      = "@GreyMatter_Bots"
+API_ID       = 20532721
+API_HASH     = "f773a77a17918242bea9907524aa7d0b"
+BOT_TOKEN    = "6284067070:AAF3t9XI_CV0K32cQXmIpKYxEdDF4V7RT64"
+SESSION      = "AQE8Q-cAeaU_A7EJgWV60L1EVUsE7dr74nzTpNigRdBqRAwfHa9kWhcpcEyVbkb2wLBLg-LNIqILweLCJRRKoO-LsTLrPHlJae9cTAhd34qlTP6CjspFrya8ILu0hb68n1ULcHdqEK2fyrFlNGpgcHdEV37xke2hcGOC_Iq1G-IJhbls_5_MQjNZVCjndro7RjPr6jd9UI5d5_0bdNDqugbyW9IR1C9sr3UI8YQlb0RswsukZMtO9JtsYlEmegWf2wyewZIyYiU-QlnnRP6XOnSzkT-oSm_FyqYkhfXC2hyorNogi5gfPUZPqfo51VCyLk8VBITso4jbXgy85qTrV3HlftjFvwAAAAFReCveAA"
+DATABASE_URI = "mongodb+srv://TestSearch:TestSearch@cluster0.y0wf0hl.mongodb.net/?retryWrites=true&w=majority"
+LOG_CHANNEL  = -1001823147516
+ADMIN        = 5758136369
+CHANNEL      = "@TazHD"
