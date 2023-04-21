6bb5f76f1b8bbcb1fc26e13a0a9783a80703be3e
LazyHubBot <131406457+LazyHubBot@users.noreply.github.com>
2023-04-21 14:15:39 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
 API_ID       = 20532721
 API_HASH     = "f773a77a17918242bea9907524aa7d0b"
-BOT_TOKEN    = "6284067070:AAF3t9XI_CV0K32cQXmIpKYxEdDF4V7RT64"
+BOT_TOKEN    = "5743072545:AAHu0Mkb1UXJVfxVkuXcdS7g5ItLB9kJAoM"
 SESSION      = "AQE8Q-cAeaU_A7EJgWV60L1EVUsE7dr74nzTpNigRdBqRAwfHa9kWhcpcEyVbkb2wLBLg-LNIqILweLCJRRKoO-LsTLrPHlJae9cTAhd34qlTP6CjspFrya8ILu0hb68n1ULcHdqEK2fyrFlNGpgcHdEV37xke2hcGOC_Iq1G-IJhbls_5_MQjNZVCjndro7RjPr6jd9UI5d5_0bdNDqugbyW9IR1C9sr3UI8YQlb0RswsukZMtO9JtsYlEmegWf2wyewZIyYiU-QlnnRP6XOnSzkT-oSm_FyqYkhfXC2hyorNogi5gfPUZPqfo51VCyLk8VBITso4jbXgy85qTrV3HlftjFvwAAAAFReCveAA"
 DATABASE_URI = "mongodb+srv://TestSearch:TestSearch@cluster0.y0wf0hl.mongodb.net/?retryWrites=true&w=majority"
 LOG_CHANNEL  = -1001823147516
 ADMIN        = 5758136369
-CHANNEL      = "@TazHD"
+CHANNEL      = "@LazYHuB"
