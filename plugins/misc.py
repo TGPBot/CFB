549b783ff2ab3ba2c5cddc4612f19170ceb71310
LazyHubBot <131406457+LazyHubBot@users.noreply.github.com>
2023-04-21 15:05:44 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -46,9 +46,8 @@ async def misc(bot, update):
     if data=="home":
        await update.message.edit(text=script.START.format(update.from_user.mention),
                                  disable_web_page_preview=True,
-                                 reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('➕ Aᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ Gʀᴏᴜᴘ ➕', url=f'http://t.me/PostSearch_Bot?startgroup=true')
-            ],[InlineKeyboardButton("ℹ️ Hᴇʟᴘ", callback_data="misc_help"),
-                                                            InlineKeyboardButton("🧑🏻‍💻 Aʙᴏᴜᴛ", callback_data="misc_about")]]))
+                                 reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("ℹ️ Hᴇʟᴘ", callback_data="misc_help"),
+                                                            InlineKeyboardButton("🧑🏻‍💻 Aʙᴏᴜᴛ", callback_data="misc_about")]])) 
     elif data=="help":
        await update.message.edit(text=script.HELP, 
                                  disable_web_page_preview=True,
